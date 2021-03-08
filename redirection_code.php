44b10394e3c7908d8f0cc3b62cbbffb777bed117
wphooks <80279525+wphooks@users.noreply.github.com>
2021-03-08 23:10:13 +0530

    Create redirection_code.php

---
+++ redirection_code.php
@@ -0,0 +1,23 @@
+function Tutor_redirect_users_by_role() {
+ 
+    $current_user   = wp_get_current_user();
+    $role_name      = $current_user->roles[0];
+ 
+    if ( 'subscriber' === $role_name ) {
+        wp_redirect( 'https://dev.wpdevcloud.com/dashboard' );
+    } // if
+ 
+} // Tutor_Student_redirect_users_by_role
+add_action( 'admin_init', 'Tutor_redirect_users_by_role' );
+
+function Tutor_redirect_users_by_role_two() {
+ 
+    $current_user   = wp_get_current_user();
+    $role_name      = $current_user->roles[0];
+ 
+    if ( 'tutor_instructor' === $role_name ) {
+        wp_redirect( 'https://dev.wpdevcloud.com/dashboard' );
+    } // if
+ 
+} // Tutor_Student_redirect_users_by_role
+add_action( 'admin_init', 'Tutor_redirect_users_by_role_two' );
